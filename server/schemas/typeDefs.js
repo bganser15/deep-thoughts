8033030a2378d9b2971a717e365014ec1c2b55e7
Brittany Ganser <15brganser@gmail.com>
2022-06-22 22:22:34 -0600

    added queries in GraphQL

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,40 @@
 // import the gql tagged template function
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
+//typeQuery puts each Thought into an array named thoughts: because each thought has multiple data types
 // create our typeDefs
+
 const typeDefs = gql`
-type Query {
-    helloWorld: String
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
   }
 `;
 
